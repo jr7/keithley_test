eca252faab3535dc957e941d6358292d7fa3aec3
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2016-04-01 20:15:38 +0200

    Speedup read querys.

--- read_keithley.py
+++ read_keithley.py
@@ -18,10 +18,9 @@ def main():
 
     #print dev.query('*RST')
     dev.write("*rst; status:preset; *cls")
-    print dev.query('*IDN?')
+    print "Reading Device: " + dev.query('*IDN?')
 
     dev.write(":SENSE:FUNCTION 'VOLT:DC'")
-
     dev.write(":FORMAT:ELEMENT READ")
     dev.write(":SYSTEM:AZERO:STATE OFF")
     dev.write(":SENSE:VOLT:DC:AVERAGE:STATE OFF")
@@ -31,13 +30,13 @@ def main():
     dev.write(":TRIGGER:COUNT 1")
     dev.write(":SAMPLE:COUNT 1")
     dev.write(":TRIGGER:DELAY 0.0")
+    dev.write(":DISP:ENABLE 0")
 
     fig = plt.figure()
     ax1 = fig.add_subplot(1,1,1)
 
-    N = 1000
+    N = 50
     data = deque([0]*N, maxlen=N)
-
 
     line, = plt.plot(data, 'bo')
     ax1.set_ylim(-1, 6)
@@ -45,13 +44,11 @@ def main():
     def animate(i):
         results = dev.ask('READ?').split('#,')
         results = [x.split(',')[0] for x in results]
-
-
         new_data = [float((x.split('VDC')[0][1:])) for x in results]
         data.extend(new_data)
         line.set_ydata(data)
 
-    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    ani = animation.FuncAnimation(fig, animate, frames=20)
     plt.show()
 
 if __name__=='__main__':
