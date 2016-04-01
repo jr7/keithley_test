54d45983dddb8cb8c30c7a428552a3cf3c4441fb
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2016-04-01 16:16:37 +0200

    Add speedup to reads and better animation.

--- read_keithley.py
+++ read_keithley.py
@@ -1,32 +1,55 @@
 import visa
 import numpy as np
 import time
+
 import matplotlib
 import matplotlib.animation as animation
 matplotlib.use('Qt4Agg')
 
+
+from collections import deque
 import matplotlib.pyplot as plt
 
 def main():
     rm = visa.ResourceManager('@py')
     device_name = rm.list_resources()[0]
 
-    dev = rm.open_resource(device_name, read_termination='\r', send_end=True)
+    dev = rm.open_resource(device_name, read_termination='\r', send_end=True, baud_rate=19200)
 
+    #print dev.query('*RST')
+    dev.write("*rst; status:preset; *cls")
     print dev.query('*IDN?')
+
+    dev.write(":SENSE:FUNCTION 'VOLT:DC'")
+
+    dev.write(":FORMAT:ELEMENT READ")
+    dev.write(":SYSTEM:AZERO:STATE OFF")
+    dev.write(":SENSE:VOLT:DC:AVERAGE:STATE OFF")
+    dev.write(":SENSE:VOLT:DC:NPLC 0.01")
+    dev.write(":SENSE:VOLT:DC:RANGE 10")
+    dev.write(":SENSE:VOLT:DC:DIGITS 4")
+    dev.write(":TRIGGER:COUNT 1")
+    dev.write(":SAMPLE:COUNT 1")
+    dev.write(":TRIGGER:DELAY 0.0")
 
     fig = plt.figure()
     ax1 = fig.add_subplot(1,1,1)
 
-    data = []
-    def animate(i):
-        time.sleep(0.1)
-        d = float((dev.ask('READ?').split('VDC')[0][1:]))
+    N = 1000
+    data = deque([0]*N, maxlen=N)
 
-        data.append(d)
-        x = np.arange(len(data))
-        ax1.clear()
-        ax1.plot(x, data, 'bo--')
+
+    line, = plt.plot(data, 'bo')
+    ax1.set_ylim(-1, 6)
+
+    def animate(i):
+        results = dev.ask('READ?').split('#,')
+        results = [x.split(',')[0] for x in results]
+
+
+        new_data = [float((x.split('VDC')[0][1:])) for x in results]
+        data.extend(new_data)
+        line.set_ydata(data)
 
     ani = animation.FuncAnimation(fig, animate, interval=1000)
     plt.show()
